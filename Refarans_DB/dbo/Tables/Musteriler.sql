357deb81f880b09cd93883773ad7a0fa3ab60e7a
Ceyda Simsek <147485241+ceydasimsekk@users.noreply.github.com>
2025-12-26 10:55:06 +0300

    İlk commit: SSDT projesi

---
+++ Refarans_DB/dbo/Tables/Musteriler.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Musteriler] (
+    [Id]    INT            NOT NULL,
+    [Ad]    NVARCHAR (100) NULL,
+    [Soyad] NVARCHAR (100) NULL,
+    [Email] NVARCHAR (200) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
